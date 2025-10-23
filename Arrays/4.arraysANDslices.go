3b46ebc060824ca7a13ad0bafb15c78507b792e2
Akshat Kumar <akshat8066@gmail.com>
2025-10-23 21:19:23 +0530

    Added Arrays and slice

---
+++ Arrays/4.arraysANDslices.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	/************************************************************************************************************************************************************/
+	//	ARRAYS ---> Homogenous collection of data
+	/************************************************************************************************************************************************************/
+
+	var arr[4]int	// by default 0 values are added, if datatype is false then default val is false, empty string in case of strings
+
+	fmt.Println(len(arr))
+
+	nums:=[4]int{1,2,3,4}
+
+	fmt.Println(nums)
+
+	/************************************************************************************************************************************************************/
+	//	SLICE ---> Like vectors in cpp
+	/************************************************************************************************************************************************************/
+
+	var slice[]int
+
+	fmt.Println(slice)
+	fmt.Println(len(slice))
+
+
+	var slice2 = make([]int,2)	// 2 is the default size
+
+		fmt.Println(slice2)
+		fmt.Println(len(slice2))
+
+		// cap function --> maximum number of elements that can fit
+
+		slice2 = append(slice2,2)	// adding values after initial t20 zeroes, which we declared above
+		slice2 = append(slice2,12)
+		slice2 = append(slice2,22)
+		slice2 = append(slice2,21)
+
+		fmt.Println(slice2)
+		fmt.Println(len(slice2))
+
+}
